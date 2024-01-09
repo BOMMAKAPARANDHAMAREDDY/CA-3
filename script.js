a47b1eae7c29f2588be14b19b409d09d5d50cffc
BOMMAKAPARANDHAMAREDDY <parandhama.b@kalvium.community>
2024-01-09 16:54:02 +0530

    CA 3 completed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
     let searchBar = document.getElementById("search");
     let container = document.getElementsByClassName("afterSearch")[0];
     let recipe = " ";
@@ -10,20 +10,20 @@ window.onload = function() {
             // console.log(data);
 
             const html = `
-            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" onclick="details('${meal.idMeal}')">
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" onclick="listOfIngredients('${meal.idMeal}')">
             <h2>${meal.strMeal}</h2>
             `;
-            
+
             document.getElementById('randomTile').innerHTML = html;
         });
 
-        let searchBtn = document.getElementById("searchBtn");
-        searchBtn.onclick = () => {
-            recipe = searchBar.value;
-            searchBar.value = '';
-            fetchRecipe();
-        };
-    
+    let searchBtn = document.getElementById("searchBtn");
+    searchBtn.onclick = () => {
+        recipe = searchBar.value;
+        searchBar.value = '';
+        fetchRecipe();
+    };
+
 
     document.addEventListener('keydown', function (event) {
         if (event.key === 'Enter') {
@@ -35,16 +35,16 @@ window.onload = function() {
 
     function fetchRecipe() {
         fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${recipe}`)
-        .then((res) => res.json())
-        .then((data) => displayData(data));
+            .then((res) => res.json())
+            .then((data) => displayData(data));
     }
 
-    function displayData(inputData){
+    function displayData(inputData) {
         container.innerHTML = '';
-    
+
         if (inputData.meals) {
             inputData.meals.forEach(meal => {
-                container.innerHTML += `<img src="${meal.strMealThumb}" alt="recipe image" onclick="details('${meal.idMeal}')">
+                container.innerHTML += `<img src="${meal.strMealThumb}" alt="recipe image" onclick="listOfIngredients('${meal.idMeal}')">
                 <h2>${meal.strMeal}</h2>`;
             });
         } else {
@@ -56,24 +56,24 @@ window.onload = function() {
 }
 
 
-function details(id){
+function listOfIngredients(id) {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
-    .then(res=>res.json())
-    .then(detail => {
-        let meal = detail.meals[0]
-        // console.log(meal)
-        var modal = document.getElementById("myModal");
-        var span = document.getElementsByClassName("close")[0];
-        span.onclick = function() { 
-          modal.style.display = "none";
-        }
-        var modalImg = document.getElementById("img01");
-        var captionText = document.getElementById("caption");
-        var ingredients = document.getElementById("ingredients")
-        modal.style.display = "block";
-        modalImg.src = meal.strMealThumb;
-        captionText.innerHTML = meal.strMeal;
-        ingredients.innerHTML = `
+        .then(res => res.json())
+        .then(list => {
+            let meal = list.meals[0]
+            // console.log(meal)
+            var modal = document.getElementById("myModal");
+            var span = document.getElementsByClassName("close")[0];
+            span.onclick = function () {
+                modal.style.display = "none";
+            }
+            var modalImg = document.getElementById("img01");
+            var captionText = document.getElementById("caption");
+            var ingredients = document.getElementById("ingredients")
+            modal.style.display = "block";
+            modalImg.src = meal.strMealThumb;
+            captionText.innerHTML = meal.strMeal;
+            ingredients.innerHTML = `
                 <h3>Ingredients</h3>
                 <ul>
                     <li>${meal.strCategory}</li>
@@ -87,6 +87,6 @@ function details(id){
             </div>
         </div>
         `;
-        
-    })
+
+        })
 }
